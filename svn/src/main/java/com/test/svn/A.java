fff0f780cc5d8992c924751fa9992814610375cb
levelee123 <levelee123@163.com>
2017-12-05 20:03:08 +0800

    111

---
+++ svn/src/main/java/com/test/svn/A.java
@@ -0,0 +1,11 @@
+package com.test.svn;
+
+public class A {
+    int x = 0;
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+
+}
---
+++ svn/src/main/java/com/test/svn/B.java
@@ -0,0 +1,4 @@
+package com.test.svn;
+
+public class B {
+}
